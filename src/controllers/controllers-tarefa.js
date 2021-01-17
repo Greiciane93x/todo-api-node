f53c2bc11fe8a6ecdc88da31f16aeab931f2990c
Greiciane93x <greiciane_araujo@hotmai.com>
2021-01-16 21:11:33 -0300

    mais algumas alteracoes

--- src/controllers/controllers-tarefa.js
+++ src/controllers/controllers-tarefa.js
@@ -1,10 +1,10 @@
 module.exports = (app)=> {
     app.get(`/tarefas`, (req, resp) =>  {
-        resp.send('<h1> Rota GET ativada</h1> ')
+        resp.send('<h1> Rota GET ativada - TAREFA</h1> ')
     }) 
 
     app.post(`/tarefas`, (req, resp)=> {
-        resp.send('<h1> Rota POST ativada </h1> ')
+        resp.send('<h1> Rota POST ativada- TAREFA</h1> ')
     })
 
 } 
--- src/controllers/controllers-usuario.js
+++ src/controllers/controllers-usuario.js
@@ -1,10 +1,10 @@
 module.exports = (app)=> {
     app.get(`/usuario`, (req, resp) =>  {
-        resp.send('<h1> Rota GET ativada</h1> ')
+        resp.send('<h1> Rota GET ativada - USUÁRIO</h1> ')
     }) 
 
     app.post(`/usuario`, (req, resp)=> {
-        resp.send('<h1> Rota POST ativada </h1> ')
+        resp.send('<h1> Rota POST ativada - USUÁRIO</h1> ')
     })
 
 } 
